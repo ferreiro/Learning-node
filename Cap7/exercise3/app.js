3d515dd47f86dfecc3d70887f8988413ea2e17b4
Jorge Ferreiro <me@jgferreiro.com>
2015-04-05 20:28:48 +0200

    Exercise 3 done: make different responses depending on the users id pass by parameter

--- Cap7/exercise3/app.js
+++ Cap7/exercise3/app.js
@@ -1,14 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    routes = require('./routes/index'), 
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +20,37 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+app.get('/users', function(req, res) {
+  res.end("Nothing to show");
+})
+
+app.get('/users/:id', function(req, res) {
+  var userID = req.params.id,
+      users = {
+        1: {
+          name: "Jorge",
+          email: "me@gmail.com",
+          phone: "699"
+        },
+        2: {
+          name: "Paul",
+          email: "me@paul.com",
+          phone: "5050"
+        }
+      };
+      
+  res.render('users.jade', {
+    title: "User id",
+    user: users[userID]
+  });
+
+}); 
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
